44be41b7a1a64c46e7ade7c54edcc5f4cf6cbdd6
381351 <demar4981@oslomet.no>
2024-02-17 18:15:48 +0100

    Added e-mail input validation and small fixes

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -11,32 +11,37 @@ function kjop() {
     const nrInn = document.getElementById("nr").value;
     const epostInn = document.getElementById("e-post").value;
 
-    let inputVerdier = document.querySelectorAll("input");
+    const inputVerdier = document.querySelectorAll("input");
+
     // Resetter hver inputfelt sin "output"-melding
     for (let verdi of inputVerdier) {
         document.getElementById(verdi.id + "-1").innerHTML = "";
     }
     document.getElementById("filmer-1").innerHTML = "";
 
-
     let feltFylt = true;
 
     // Sjekker om inputfeltene + dropdown er tomme, og om de er gyldige
     for (let verdi of inputVerdier) {
-        let utFelt = document.getElementById(verdi.id + "-1") //Output felt
+        let utFelt = document.getElementById(verdi.id + "-1") //Output "error" felt
         let ut = ""
-        if ((verdi.id == "antall" && verdi.value <= 0) || // Sjekker om antallet er mindre eller lik null
-            verdi.id == "nr" && verdi.value.length == 8 // Sjekker om nummeret har en lengde på 8 siffer
-            && verdi.value.match(/^[1-9]\d{7}/)) { //RegEx sjekker at nummeret ikke begynner på 0
-            // og deretter etterfulgt av 7 sifre
-            ut = "Skriv inn gyldig " + verdi.id;
+        // Sjekker om antallet er mindre eller lik null
+        if ((verdi.id == "antall" && verdi.value <= 0) ||
+            // Sjekker om nummeret har en lengde på 8 siffer
+            (verdi.id == "nr" && verdi.value.length != 8
+                //RegEx sjekker at nummeret ikke begynner på 0
+                // og deretter etterfulgt av 7 sifre
+                && !verdi.value.match(/^[1-9]\d{7}/)) ||
+            // Sjekker blant annet om e-posten ikke begynner på @
+            verdi.id == "e-post" && !verdi.value.match(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+/)) {
+            ut = "Skriv inn gyldig " + verdi.id; // Utskriftsmelding for ugyldig felt
             feltFylt = false;
         }
         if (verdi.value == "") { // Sjekker om inputfeltene er tomme
             ut = "<span> Må skrive noe i " + verdi.id + "</span>";
             feltFylt = false;
         }
-        utFelt.innerHTML = ut; //Skriver melding ut om hva som må fikses
+        utFelt.innerHTML = ut; //Skriver error-melding ut om hva som må fikses
     }
 
     // Siden dropdown-listen for filmer er av en annen datatype enn de andre inputfeltene,
@@ -58,7 +63,7 @@ function kjop() {
             epost : epostInn
         }
         let ut = "";
-        // Hvis det er aller første billett, legges til tittel på toppen
+        // Hvis det er aller første billett, legges til tabelltittel på toppen
         if (i == 0) {
             ut = "<div><div>Film</div><div>Antall</div><div>Fornavn</div><div>Etternavn</div><div>Telefon</div><div>E-post</div></div>"
         }
@@ -76,14 +81,13 @@ function kjop() {
         for (let verdi of inputVerdier) {
             verdi.value = "";
         }
-        filmer.selectedIndex = 0;
+        filmer.selectedIndex = 0; // Setter dropdown til standardverdi "Velg film"
     }
-
 }
-
+// Funksjon som tømmer arrayet og billettlisten
 function slett() {
     document.getElementById("billetter").innerHTML = "";
-    billettArray.splice(0, billettArray.length); //Sletter objektene i arrayet
+    billettArray = []; // Resetter arrayet til et tomt et
     // Resetter i slik at objekter kan fremdeles legges inn i arrayet uten å laste inn siden på nytt
     i = 0;
 }
